3847ed4596f7b4e071edd0420fff569e75b6cb6b
Rui-Jie Fang <rui.jie.fang@gmail.com>
2019-03-25 23:45:15 -0400

    Find largest component

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.TreeSet;
+
+import org.graphstream.algorithm.generator.Generator;
+import org.graphstream.algorithm.generator.RandomGenerator;
 import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.*;
 
@@ -7,50 +13,57 @@ public class Main {
         new Main();
     }
 
+    Generator gen;
     public Main() {
-        Graph graph = new SingleGraph("tutorial 1");
-
-        graph.addAttribute("ui.stylesheet", styleSheet);
-        graph.setAutoCreate(true);
-        graph.setStrict(false);
-
-        graph.addEdge("AB", "A", "B");
-        graph.addEdge("BC", "B", "C");
-        graph.addEdge("CA", "C", "A");
-        graph.addEdge("AD", "A", "D");
-        graph.addEdge("DE", "D", "E");
-        graph.addEdge("DF", "D", "F");
-        graph.addEdge("EF", "E", "F");
-
-
-        graph.display(true);
-
-        for (Node node : graph) {
-            node.addAttribute("ui.label", node.getId());
+        Graph graph = new SingleGraph("Random");
+        gen = new RandomGenerator(2.0);
+        gen.addSink(graph);
+        gen.begin();
+        for(int i=0; i<100; i++)
+            gen.nextEvents();
+        gen.end();
+        Node s = graph.getNode(findLargestComponent(graph));
+        TreeSet<Integer> cc = new TreeSet<Integer>();
+        Iterator<Node> bfsIterator = s.getBreadthFirstIterator();
+        ArrayList<Node> removedNodes = new ArrayList<Node>();
+        cc.add(s.getIndex());
+        while (bfsIterator.hasNext()) {
+            Node n = bfsIterator.next();
+            cc.add(n.getIndex());
+            System.out.println("Node " + n.getIndex() + " added");
         }
-
-        //     explore(graph.getNode("A"));
-    }
-
-    public void explore(Node source) {
-        Iterator<? extends Node> k = source.getBreadthFirstIterator();
-
-        while (k.hasNext()) {
-            Node next = k.next();
-            next.setAttribute("ui.class", "marked");
-            sleep();
+        for(Node node : graph) {
+            if (!cc.contains(node.getIndex())) {
+                removedNodes.add(node);
+            }
         }
+        for(Node node : removedNodes) {
+            graph.removeNode(node);
+        }
+        graph.display();
     }
 
-    protected void sleep() {
-        try { Thread.sleep(1000); } catch (Exception e) {}
+    int findLargestComponent(Graph graph) {
+        int largestCcSize = 0, largestCcId =0, ccId[] = new int[graph.getNodeCount()];
+        ArrayList<Integer> ccSizes = new ArrayList<Integer>();
+        Iterator<Node> bfsIterator;
+        Arrays.fill(ccId, -1);
+        for(Node node : graph) {
+            if (ccId[node.getIndex()] == -1) {
+                ccSizes.add(0);
+                ccId[node.getIndex()] = ccSizes.size() - 1;
+                bfsIterator = node.getBreadthFirstIterator();
+                while (bfsIterator.hasNext()) {
+                    Node nextNode = bfsIterator.next();
+                    ccId[nextNode.getIndex()] = ccSizes.size() - 1;
+                    ccSizes.set(ccSizes.size() - 1, ccSizes.get(ccSizes.size() - 1) + 1);
+                }
+                if (largestCcSize < ccSizes.get(ccSizes.size() - 1)) {
+                    largestCcSize = ccSizes.get(ccSizes.size() - 1);
+                    largestCcId = node.getIndex();
+                }
+            }
+        }
+        return largestCcId;
     }
-
-    protected String styleSheet =
-            "node {" +
-                    "	fill-color: black;" +
-                    "}" +
-                    "node.marked {" +
-                    "	fill-color: red;" +
-                    "}";
 }
